2d367a9f80f86663dac256a8c1e1f83b03177fe1
TreSonna <59778418+TreSonna@users.noreply.github.com>
2020-06-05 13:50:34 -0700

    add basic webpage layout, taglines, and basic styling

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const userName = prompt ("What is your Name?");
+undefined
+    userName;
+
+    alert(" Hello " + userName + " Please take this survey to determine the best language of code for you")
+
+    
